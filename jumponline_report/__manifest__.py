d5156b54444f8493a05b5b1188909103236cd00b
shj-odoo <shj@odoo.com>
2021-05-03 13:05:19 +0530

    [ADD][2516145] Add new field and method in account.move and stock.picking model for selected layout. also add views for both models.

--- jumponline_report/__manifest__.py
+++ jumponline_report/__manifest__.py
@@ -18,13 +18,15 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['sale_management'],
+    'depends': ['sale_management', 'stock'],
 
     # always loaded
     'data': [
         'views/document_views.xml',
         'views/report_templates_views.xml',
         'views/res_partner_views.xml',
-        'views/sale_order_views.xml'
+        'views/sale_order_views.xml',
+        'views/account_move_views.xml',
+        'views/stock_picking_views.xml'
     ],
 }
--- jumponline_report/models/__init__.py
+++ jumponline_report/models/__init__.py
@@ -4,3 +4,5 @@ from . import document
 from . import res_company
 from . import res_partner
 from . import sale_order
+from . import account_move
+from . import stock_picking

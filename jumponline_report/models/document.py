358c1b4d45d6e1e53f51259db14f01b6575cc524
shj-odoo <shj@odoo.com>
2021-05-10 12:32:36 +0530

    [MOD] Modify the preview_ambiance field sequence in code

--- jumponline_report/models/document.py
+++ jumponline_report/models/document.py
@@ -14,6 +14,14 @@ class BaseDocumentLayout(models.TransientModel):
     secondary_color_ambiance = fields.Char(related='company_id.secondary_color_ambiance', readonly=False)
     report_header_ambiance = fields.Text(related='company_id.report_header_ambiance', readonly=False)
     report_footer_ambiance = fields.Text(related='company_id.report_footer_ambiance', readonly=False)
+    preview_ambiance = fields.Html(compute='_compute_preview_ambiance',
+                                   sanitize=False,
+                                   sanitize_tags=False,
+                                   sanitize_attributes=False,
+                                   sanitize_style=False,
+                                   sanitize_form=False,
+                                   strip_style=False,
+                                   strip_classes=False)
 
     @api.depends('report_layout_id', 'logo_ambiance', 'secondary_layout', 'report_header_ambiance',
                  'report_footer_ambiance')
@@ -28,15 +36,6 @@ class BaseDocumentLayout(models.TransientModel):
                                                                       {'company': wizard, 'preview_css': preview_css})
             else:
                 wizard.preview_ambiance = False
-
-    preview_ambiance = fields.Html(compute='_compute_preview_ambiance',
-                                   sanitize=False,
-                                   sanitize_tags=False,
-                                   sanitize_attributes=False,
-                                   sanitize_style=False,
-                                   sanitize_form=False,
-                                   strip_style=False,
-                                   strip_classes=False)
 
     @api.depends('report_layout_id', 'logo', 'font', 'primary_color', 'secondary_color', 'report_header',
                  'report_footer', 'secondary_layout')

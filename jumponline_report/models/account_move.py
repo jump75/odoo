8fe0e01ee5867fd48e14920b215d410bb4ee9a35
shj-odoo <shj@odoo.com>
2021-05-10 12:20:45 +0530

    [MOD] Modify the code for print report template, and also take chages in sale order, account.move and stock picking for selected layout field.

--- jumponline_report/models/account_move.py
+++ jumponline_report/models/account_move.py
@@ -7,9 +7,3 @@ class AccountMove(models.Model):
     _inherit = "account.move"
 
     selected_layout = fields.Selection([('ambiance', 'Ambiance Deco'), ('jump', 'Jump Online')])
-
-    @api.onchange('partner_id')
-    def set_default_selected_layout(self):
-        self.ensure_one()
-        if self.partner_id:
-            self.selected_layout = self.partner_id.selected_layout
